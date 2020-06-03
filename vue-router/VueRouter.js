6eb4b6dbace5113ba4b553f734b5ada9e4dbe08a
CodeYHJ <colgateyhj@gmail.com>
2020-06-03 17:41:36 +0800

    feat(roter): router

--- vue-router/VueRouter.js
+++ vue-router/VueRouter.js
@@ -6,13 +6,34 @@ class VueRouter {
     if (!Array.isArray(options.routers)) throw new Error("routers must be Array");
     this.routers = options.routers;
     this.current = null
+    this.comContain=[]
     window.addEventListener("hashchange", (el) => {
-      console.log(el);
-      console.log(this)
-      this.app._route = this.routers[1]
+      const {newURL}=el
+      const target = newURL.split('#')
+      if(target.length>0){
+        const t = target[1]
+        if(t==='a'){
+          this.update(this.routers[0])
+          // this.app._route = this.routers[0]
+        }else if(t==='b'){
+          this.update(this.routers[1])
+
+          // this.app._route = this.routers[1]
+        }
+        else if(t==='c'){
+          this.update(this.routers[2])
+
+          // this.app._route = this.routers[2]
+        }
+      }
     });
   }
   init(vueCompentInstance) {
-    this.app = vueCompentInstance
+    this.comContain.push(vueCompentInstance)
+  }
+  update(com){
+    this.comContain.forEach(route=>{
+      route._route=com
+    })
   }
 }

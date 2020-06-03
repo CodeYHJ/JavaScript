61a7e62fb3a6630b9fe203e135d27fed6b768fab
CodeYHJ <colgateyhj@gmail.com>
2020-06-03 16:28:46 +0800

    feat(vuerouter): vuereouter

--- vue-router/VueRouter.js
+++ vue-router/VueRouter.js
@@ -1,8 +1,18 @@
 class VueRouter {
-  constructor() {
+  constructor(options) {
+    if (!options)
+      throw new Error("must be set options in new VueRouter(options)");
+    if (!options.routers) throw new Error("must be set routers in options");
+    if (!Array.isArray(options.routers)) throw new Error("routers must be Array");
+    this.routers = options.routers;
+    this.current = null
     window.addEventListener("hashchange", (el) => {
       console.log(el);
+      console.log(this)
+      this.app._route = this.routers[1]
     });
   }
-  init() {}
+  init(vueCompentInstance) {
+    this.app = vueCompentInstance
+  }
 }
